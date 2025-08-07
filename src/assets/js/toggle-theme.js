e25c6d0c09988b1398fd79f98b911187afb6de3e
Erick Fellipe <efrc22br@gmail.com>
2025-08-07 16:29:06 -0300

    Adicionado bordas inferiores a nav bar mobile, adicionado animação no botão toggle de mudar de modo claro para escuro e adicionado local storage para salvar modos selecionados

--- src/assets/js/toggle-theme.js
+++ src/assets/js/toggle-theme.js
@@ -5,14 +5,39 @@ const darkModeImage = 'src/assets/imgs/icons/moon.svg';
 const dataTheme = document.documentElement;
 const themeIcons = document.querySelectorAll('img[data-icon]');
 
+
+//Salvar modo ao carregar página
+
+const savedTheme = localStorage.getItem('theme') || 'light';
+dataTheme.setAttribute('data-theme', savedTheme)
+
+if (savedTheme === 'dark'){
+    toggleIcon.src = darkModeImage
+    toggleIcon.alt = 'Ícone de lua, Modo Escuro'
+} else{
+    toggleIcon.src = lightModeImage
+    toggleIcon.alt = 'Ícone de sol, Modo Claro'
+}
+
+
 toggleButton.addEventListener("click", () => {
     const currentTheme = dataTheme.getAttribute('data-theme');
+
+    toggleIcon.classList.add("fade-out")
+    setTimeout(() =>{
 
     if (currentTheme === 'light'){
         dataTheme.setAttribute('data-theme', 'dark');
         toggleIcon.src = darkModeImage;
+        toggleIcon.alt = "Ícone de lua, Modo Escuro"
+        localStorage.setItem('theme', 'dark');
     } else{
         dataTheme.setAttribute('data-theme', 'light');
         toggleIcon.src = lightModeImage;
+        toggleIcon.alt = "Ícone de sol, Modo claro"
+        localStorage.setItem('theme', 'light');
     }
+
+    toggleIcon.classList.remove("fade-out")
+    }, 300);
 });
